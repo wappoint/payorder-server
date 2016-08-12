3fb50abba487a6556ca906b895786511f5d33d2b
676002187 <676002187@qq.com>
2016-08-12 10:50:01 +0800

    构建项目

---
+++ src/main/java/com/wapoint/Application.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.wapoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiazhang
+ * @date 2016年8月12日
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
